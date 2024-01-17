5f484a3d04f5ced2043cb14f30755a73544610bd
matamancos3000 <135998550+matamancos3000@users.noreply.github.com>
2024-01-17 09:23:20 -0600

    se crea nueva rama con templete html

--- Indicadores/urls.py
+++ Indicadores/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reportes/', include('reportes.urls'))
+    path('reportes/', include('reportes.urls')),
+    path('/', include('reportes.urls'))
 ]
