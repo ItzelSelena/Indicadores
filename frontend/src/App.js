7f628e77dbb2b31a7344cfb7c30edbf0655b2755
matamancos3000 <135998550+matamancos3000@users.noreply.github.com>
2024-01-21 14:15:55 -0600

    fondo rojo y logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,18 @@
-//import logo es el logo de react ya venia cuando se creó el proyecto
-import logo from './logo.svg';
-import './App.css';
-//import MainDash from './components/MainDash/MainDash';
+import './App.css'
+import MainDash from './components/MainDash/MainDash';
+import RightSide from './components/RigtSide/RightSide';
+import Sidebar from './components/Sidebar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!! prueba :3 prueba 2 prueba 3 prueba 4!!!!!!!!!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React aiuda
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-
-/*
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
+        <Sidebar/>
         <MainDash/>
+        <RightSide/>
       </div>
     </div>
   );
 }
 
 export default App;
-
-*/
